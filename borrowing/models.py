bf01a9f2098a0231a82f20c33f05e1e2e80d5bac
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-08-12 12:38:22 +0300

    add borrowing model with validations & fix import in admin.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,38 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from book.models import Book
+from library_service import settings
 
 
 class Borrowing(models.Model):
-    pass
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def clean(self):
+        if self.expected_return_date < self.borrow_date:
+            raise ValidationError(_(
+                "Expected return date cannot be earlier than borrow date."
+            ))
+        if self.actual_return_date:
+            if self.actual_return_date < self.borrow_date:
+                raise ValidationError(_(
+                    "Actual return date cannot be earlier than borrow date."
+                ))
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.book.title} borrowed on {self.borrow_date}"
+
+    class Meta:
+        ordering = ["expected_return_date"]
