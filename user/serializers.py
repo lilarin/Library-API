b2ee999fcc21acccfccf67ba85d01a47449d60db
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-13 11:06:31 +0300

    add update user serializer, view, url

--- user/serializers.py
+++ user/serializers.py
@@ -14,3 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- user/views.py
+++ user/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
-    permission_classes = []
+    permission_classes = ()
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
