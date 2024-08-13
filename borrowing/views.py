2f617b9b67f45e0cafa9e0d876d73c5553276a00
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-13 16:08:37 +0200

    add return borrowing in BorrowingViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,17 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.mixins import (
     ListModelMixin,
     RetrieveModelMixin,
     CreateModelMixin
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowing.filters import BorrowingFilter
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingListReadSerializer,
     BorrowingRetrieveReadSerializer,
-    BorrowingCreateSerializer
+    BorrowingCreateSerializer,
+    BorrowingReturnSerializer
 )
 
 
@@ -41,3 +44,17 @@ class BorrowingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["patch"], url_path="return", permission_classes=[IsAuthenticated])
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+        serializer = BorrowingReturnSerializer(
+            borrowing,
+            data=request.data,
+            context={"request": request},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
