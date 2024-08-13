81db0dd78f0125f03246d20ae2d64c781e399324
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-13 15:24:03 +0300

    add tests for user & fixes

--- user/views.py
+++ user/views.py
@@ -1,7 +1,5 @@
 from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
