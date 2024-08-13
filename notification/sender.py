7f939cb7182a22134c5419406897f2ef35835dc9
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-13 11:54:51 +0300

    Set up telegram notifications

---
+++ notification/sender.py
@@ -0,0 +1,19 @@
+import os
+from django.contrib.sites import requests
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+token = os.environ.get("TELEGRAM_BOT_TOKEN")
+chat_id = os.environ.get("TELEGRAM_NOTIFICATION_CHANNEL")
+
+
+def send_message(message):
+    if token and chat_id:
+        try:
+            requests.post(
+                url="https://api.telegram.org/bot{0}/{1}".format(token, "sendMessage"),
+                data={'chat_id': chat_id, 'text': message}
+            )
+        except requests.exceptions.RequestException as error:
+            print(f"An error occurred while sending message to TelegramAPI: {error}")
