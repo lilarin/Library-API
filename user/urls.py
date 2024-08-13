ded2d5823c6d026e5836e512acee6bcfab66ec21
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-08-13 10:53:37 +0300

    add create user view and url for it

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from user.views import CreateUserView
+
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = []
