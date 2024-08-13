de39b34b12e50e18e6169b177118791a726a3165
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-08-13 14:19:06 +0300

    add urls for list and detail page

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = []
+from borrowing.views import BorrowingListView, BorrowingDetailView
+
+urlpatterns = [
+    path("", BorrowingListView.as_view(), name="borrowing-list"),
+    path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"),
+]
 
 app_name = "borrowing"
