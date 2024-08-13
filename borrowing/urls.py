269e8edbe9a8fd6d12b48129ee2e9e79c16b745c
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-13 11:07:28 +0200

    edit permissions in views.py

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -4,7 +4,7 @@ from borrowing.views import BorrowingCreateView
 
 
 urlpatterns = [
-    path("borrowings/", BorrowingCreateView.as_view(), name="borrowing-create"),
+    path("", BorrowingCreateView.as_view(), name="borrowing-create"),
 ]
 
 app_name = "borrowing"
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Borrowing
 from .serializers import BorrowingSerializer
 
@@ -6,6 +8,7 @@ from .serializers import BorrowingSerializer
 class BorrowingCreateView(generics.CreateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
