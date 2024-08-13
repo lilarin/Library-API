0e10d7c1f11223a8490ea1eac5ce4ea81762fcb2
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-14 01:40:58 +0300

    Fixes of the route accepted methods and permissions. Additionally added filtering

--- book/views.py
+++ book/views.py
@@ -1,16 +1,32 @@
 from rest_framework import viewsets
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
+
+from book.filters import BookFilter
 from book.models import Book
-from book.serializers import BookSerializer, BookDetailSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from book.permissions import IsAdminOrReadOnly
+from book.serializers import (
+    BookSerializer,
+    BookDetailSerializer
+)
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(
+    viewsets.GenericViewSet,
+    ListModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+):
     queryset = Book.objects.all()
+    permission_classes = (IsAdminOrReadOnly,)
+    filterset_class = BookFilter
 
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BookDetailSerializer
-        else:
-            return BookSerializer
-
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+        return BookSerializer
