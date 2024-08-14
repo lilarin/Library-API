6bce61e355d55ab8927eb37866ceecca0ff13abd
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-14 15:20:45 +0200

    fix bug with return book

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -91,6 +91,11 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
 
 
 class BorrowingReturnSerializer(serializers.ModelSerializer):
+    def validate_actual_return_date(self, value):
+        if value is None:
+            raise serializers.ValidationError("The return date cannot be null.")
+        return value
+
     def validate(self, attrs):
         if self.instance.actual_return_date:
             raise serializers.ValidationError(
