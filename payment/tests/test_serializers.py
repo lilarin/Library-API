60faba480c19355c022fec6a15a9327195400b29
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-15 16:08:40 +0300

    Tests fixes

--- payment/tests/test_serializers.py
+++ payment/tests/test_serializers.py
@@ -10,26 +10,9 @@ from django.utils import timezone
 class PaymentSerializerTests(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            email="testuser@test.com", password="test1password"
-        )
-        self.book = Book.objects.create(
-            title="Test Book",
-            author="Test Author",
-            cover="SOFT",
-            inventory=5,
-            daily_fee="1.00"
-        )
-        self.borrowing = Borrowing.objects.create(
-            borrow_date=timezone.now().date(),
-            expected_return_date=timezone.now().date() + timezone.timedelta(days=1),
-            book=self.book,
-            user=self.user
-        )
         self.payment = Payment.objects.create(
             status=Payment.Status.PENDING,
             payment_type=Payment.Type.PAYMENT,
-            borrowing=self.borrowing,
             session_url="https://payment-provider.com/session/test",
             money_to_pay="100.00"
         )
@@ -38,7 +21,6 @@ class PaymentSerializerTests(TestCase):
         serializer = PaymentSerializer(instance=self.payment)
         data = serializer.data
         self.assertEqual(data["status"], Payment.Status.PENDING)
-        self.assertEqual(data["payment_type"], Payment.Type.PAYMENT)
         self.assertEqual(data["money_to_pay"], "100.00")
         self.assertEqual(data["session_url"], "https://payment-provider.com/session/test")
 
