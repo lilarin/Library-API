3dde0bcfbd5914b22ac2f38f9c915502d1e8afac
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-15 14:03:46 +0200

    some fix

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
