5bf73595c7c0ddbfb3d761084be693fed40f32ed
unlie9 <unlie.devops9@gmail.com>
2024-08-18 16:52:41 +0300

    fixed flake8

--- book/serializers.py
+++ book/serializers.py
@@ -5,22 +5,30 @@ from book.models import Book
 class BookSerializer(serializers.ModelSerializer):
     def validate_inventory(self, value):
         if value < 1:
-            raise serializers.ValidationError("Inventory must be at least 1.")
+            raise serializers.ValidationError(
+                "Inventory must be at least 1."
+            )
         return value
 
     def validate_daily_fee(self, value):
         if value < 0.09:
-            raise serializers.ValidationError("Daily fee must be at least 0.09.")
+            raise serializers.ValidationError(
+                "Daily fee must be at least 0.09."
+            )
         return value
 
     def validate_title(self, value):
         if len(value) < 3 or value.isdigit():
-            raise serializers.ValidationError("Send correct title.")
+            raise serializers.ValidationError(
+                "Send correct title."
+            )
         return value
 
     def validate_author(self, value):
         if value.isdigit() or len(value) < 3 or len(value) > 255:
-            raise serializers.ValidationError("Send correct name.")
+            raise serializers.ValidationError(
+                "Send correct name."
+            )
         return value
 
     class Meta:
